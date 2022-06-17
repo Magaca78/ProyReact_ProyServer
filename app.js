60fad8863bb03baffb4bf9c334b33d373097bd82
Magaca78 <mateo.1701920666@ucaldas.edu.co>
2022-06-16 22:01:54 -0500

    pre crud

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const cors = require("cors")
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const { API_VERSION } = require("./config");
 
 const userRoutes = require("./src/routes/user");
+const authRoutes = require("./src/routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -15,6 +16,7 @@ app.use(cors())
 
 /* Creación de los endpoint del proyecto */
 app.use(`/api/${API_VERSION}`, userRoutes);
+app.use(`/api/${API_VERSION}`, authRoutes);
 
-/* Condiguración de los header HTTP */
+/* Configuración de los header HTTP */
 module.exports = app;
